17bb58a32861936c52eff9b5668eed2dd0f09b11
AEGbhangr <86587574+AEGbhangr@users.noreply.github.com>
2021-06-28 11:26:26 -0400

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
+  /*
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'aegbhangra.nyc@gmail.com',
     'password' => 'AnakhE123',
     'port' => '465'
   );
-  
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
